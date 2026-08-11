6e7ccf9117f51d4d0a68e49301a1c45a6492e1c2
vikrant <97667653+TERNION-1121@users.noreply.github.com>
2026-08-11 22:17:09 +0530

    initiate flask implementation of website

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-with open("iisers/iisertvm.html", "r+") as f:
-	contents = f.read().replace("**", "<strong>")
-	f.write(contents)
-	print(contents)
-print("changed successfully")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+	return render_template("index.html")
+
+@app.route("/institutes")
+def institutes():
+	return render_template("institutes.html")
+
+@app.route("/contribute")
+def contribute():
+	return render_template("contribute.html")
+
+@app.route("/iisertvm")
+def iisertvm():
+	return render_template("iisers/iisertvm.html")
+
+app.run(debug=True)
